115640b950684dd4373d243773927bcba1a69385
feng <feng2860984180@163.com>
2022-03-24 00:23:33 +0800

    feat: 组件化开发-React通过jsx实现slot插槽-属性形式优化

--- 06-learn-component/src/05-React方式来实现slot-插槽/App.js
+++ 06-learn-component/src/05-React方式来实现slot-插槽/App.js
@@ -3,14 +3,21 @@ import React, { Component } from 'react'
 // 缺点: 位置必须固定
 import NavBar from './NavBar'
 
+// 第二种方式: 使用指定参数属性传入需要展示的标签数据
+import NavBarNew from './NavBar-new'
+
 export default class App extends Component {
   render() {
     return (
-      <NavBar>
-        <span>left</span>
-        <span>center</span>
-        <span>right</span>
-      </NavBar>
+      <div>
+        <NavBar>
+          <span>left</span>
+          <span>center</span>
+          <span>right</span>
+        </NavBar>
+        <br />
+        <NavBarNew leftSlot={'左侧'} rightSlot={'右侧'} />
+      </div>
     )
   }
 }
