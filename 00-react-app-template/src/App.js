93abec491b2b126268b4e265912864c711517301
feng <feng2860984180@163.com>
2022-04-07 16:26:38 +0800

    feat: 抽取了一个 react-app 的模板-方便学习使用

---
+++ 00-react-app-template/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  componentDidMount() {
+    console.log('App 组件挂载成功')
+  }
+
+  render() {
+    return <div>React App Template</div>
+  }
+}
