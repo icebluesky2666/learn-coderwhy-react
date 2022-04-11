19e638217357e57be42d6cf4c8fba90d209678e4
feng <feng2860984180@163.com>
2022-04-11 23:35:38 +0800

    feat: NavLink-Routes-Route-组件基本演练

--- 15-react-router/src/App.js
+++ 15-react-router/src/App.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, NavLink, Route, Routes } from 'react-router-dom'
 import About from './pages/about'
 import Home from './pages/home'
 import Profile from './pages/profile'
+import NoMatch from './pages/noMatch'
+import User from './pages/user'
 
+import './App.css'
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      links: [
+        {
+          to: '/',
+          title: '首页'
+        },
+        {
+          to: '/about',
+          title: '关于'
+        },
+        {
+          to: '/profile',
+          title: '我的'
+        },
+        {
+          to: '/User',
+          title: '用户'
+        },
+        {
+          to: '/abc/noMatch',
+          title: '啥都不匹配'
+        }
+      ]
+    }
+  }
+
   componentDidMount() {
     console.log('App 组件挂载成功')
   }
 
   render() {
+    console.log(this.props)
+
     return (
       <div>
         <h2>React App Template</h2>
@@ -17,14 +50,30 @@ export default class App extends Component {
         <hr />
 
         <BrowserRouter>
-          <Link to="/">首页 </Link>
-          <Link to="/about">关于 </Link>
-          <Link to="/profile">我的 </Link>
+          <nav>
+            {this.state.links.map((item) => {
+              return (
+                <li key={item.title}>
+                  <NavLink to={item.to} className={({ isActive }) => (isActive ? 'link-active' : '')}>
+                    {item.title}
+                  </NavLink>
+                </li>
+              )
+            })}
+          </nav>
+
+          {/* <Link to="/about">关于 </Link> */}
+          {/* <Link to="/profile">我的 </Link> */}
 
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/profile" element={<Profile />} />
+            {/* 路由匹配 */}
+            <Route path="/user" element={<User />} />
+            {/* <Route path="/:id" element={<User />} /> */}
+            {/* 没有匹配上的 */}
+            <Route path="*" element={<NoMatch />} />
           </Routes>
         </BrowserRouter>
       </div>
---
+++ 15-react-router/src/pages/noMatch.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class NoMatch extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h2>我是 NoMatch 组件: 啥也没有匹配上</h2>
+      </div>
+    )
+  }
+}
